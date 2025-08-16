5217aa0329e42a6680ffbcabf3e72e5c0bee8b20
Ranjani <ranjani.shivashankar@gmail.com>
2025-08-16 06:56:59 +0530

    Added a new vue file and package json

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+  export default{
+    // component options
+    // reactive state defined here
+    data(){
+      return {
+        title: "Art Gallery",
+        heading: "Welcome to my website!",
+        description: "Hey all. Welcome welcome! 🤗 Trying to see if i can set up a web based gallery for all my digital artworks",
+        copyright: "All rights reserved ©️ AninaRJ  2025"
+      }
+    }
+  }
+</script>
+
+<template>
+<title>
+  {{title}}
+</title>
+<header>
+  {{heading}}
+  <p>
+    <!--Tab to edit-->
+    {{description}}
+  </p>
+</header>
+
+<footer>
+  {{copyright}}
+</footer>
+</template>
