dfb9134490c1823f93fc175739e1ce31f063e7ea
AninaRJ <ranjani.shivashankar@gmail.com>
2025-08-17 14:03:33 +0530

    update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,16 @@
+<script>
+  import Header from 'sections/Header'
+  import Footer from 'sections/Footer'
+</script>
+
 <template>
-  <!--Tab to edit-->
+  <Header>
+  </Header>
   <div>
     <!--Tab to edit-->
     Home Page
   </div>
+  <Footer>
+    <!--Tab to edit-->
+  </Footer>
 </template>
