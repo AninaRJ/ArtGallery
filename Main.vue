4e97c71b323b5071983d24c632a323ccdae463c2
AninaRJ <ranjani.shivashankar@gmail.com>
2025-08-18 10:46:29 +0530

    Check if mounting is happening

---
+++ Main.vue
@@ -0,0 +1,3 @@
+<template>
+  Inside main vue
+</template>
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 // import the root component App from a single-file component.
 import App from './App.vue'
+import Main from './Main.vue';
 
-const app = createApp(App)
+const app = createApp(Main)
 app.mount('#app')
